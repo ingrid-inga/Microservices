2d6fc568f86edaeb4665f1a164df72fac3bdfc1b
ingrid-inga <ingridinga10@gmail.com>
2022-08-24 15:24:30 -0300

    stay testing

--- car-service/src/main/java/com/example/carservice/CarServiceApplication.java
+++ car-service/src/main/java/com/example/carservice/CarServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CarServiceApplication {
